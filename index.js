2daddd4cafcea32c5021ad3cd2924daf5da836bc
salmi <toni.salminen89@gmail.com>
2020-11-17 17:36:50 +0200

    modify

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors);
-
+const db = [{ name: "duumi" }, { name: "pappa" }];
+app.get("/names", (req, res) => {
+  res.send(db);
+});
 function main() {
-  const db = [{ name: "duumi" }, { name: "pappa" }];
-  app.get("/names", (req, res) => {
-    res.send(db);
-  });
   const port = process.env.PORT || 8080;
   const server = app.listen(port, async () => {
     try {
-      //await database.connect();
       console.log("Database connection successful");
       console.log(`Listening on port ${server.address().port}`);
     } catch (err) {
