a36f326850282190a6755b4dac659e8a5656ece5
salmi <toni.salminen89@gmail.com>
2020-11-18 21:31:58 +0200

    modify index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const api = require("./routes/api.js");
+const database = require("./database/crud.js");
 
 app.use(cors());
 app.use("/api", api);
@@ -10,6 +11,7 @@ function main() {
   const port = process.env.PORT || 8080;
   const server = app.listen(port, async () => {
     try {
+      //await database.connect();
       console.log("Database connection successful");
       console.log(`Listening on port ${server.address().port}`);
     } catch (err) {
