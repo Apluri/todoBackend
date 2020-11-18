835ca2928b1248f7d87603098cd8ddb2a9f8ef86
salmi <toni.salminen89@gmail.com>
2020-11-18 21:39:29 +0200

    setup basic framework for router middleware

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,67 @@
 const express = require("express");
 const router = express.Router();
-// const validate = require("jsonschema").validate;
-// const sqlConnection = require("../database/crudrepository.js");
-
-// automatically convert to json each fetch?
 router.use(express.json());
+// router.use(express.static("../../todoFrontend/?"));
+const validator = require("..model/validator.js");
+const sqlConnection = require("../database/crud.js");
+const task = require("..model/todoTask.js");
 
 let db = [
   { id: 1, task: "Mee toihi", deadline: null },
   { id: 2, task: "Tiskaa", deadline: null },
 ];
 
-// send json table
-router.get("/tasks", (req, res) => {
-  res.send(db);
+// get all tasks
+router.get("/tasks", async (req, res) => {
+  try {
+    res.statusCode = 200;
+    res.send(await sqlConnection.findAll());
+  } catch (err) {
+    req.statusCode = 500;
+    res.end();
+  }
+  //res.send(db);
 });
 
-// post to local database
-router.post("/", (req, res) => {
-  db.push(req.body);
+// add new task
+router.post("/tasks", async (req, res) => {
+  // let temp = new task(req.body.?, req.body.?, req.body.?);
+  let validationResult = validator.taskValidation(temp);
+  try {
+    if (validationResult.valid) {
+      try {
+        res.statusCode = 201;
+        res.send(await sqlConnection.save(temp));
+      } catch (err) {
+        res.statusCode = 400;
+        res.sendStatus(400);
+      }
+    } else {
+      res.statusCode = 406;
+      res.send(validationResult.errors);
+    }
+  } catch (err) {
+    res.statusCode = 400;
+    res.send(err);
+  }
+  //db.push(req.body);
 });
 
-// delete from local database
-router.delete("/:taskid([0-9]+)", (req, res) => {
-  db = db.filter((todoTask) => todoTask.id !== Number(req.params.taskid));
+// delete by id
+router.delete("/tasks/:taskid([0-9]+)", async (req, res) => {
+  if (validator.idValidation(Number(req.params.taskid))) {
+    try {
+      await sqlConnection.deleteById(Number(req.params.taskid));
+      res.statusCode = 204;
+      res.sendStatus(204);
+    } catch (err) {
+      res.statusCode = 400;
+      res.send(err);
+    }
+  } else {
+    res.statusCode = 406;
+    res.sendStatus(406);
+  }
+  //db = db.filter((todoTask) => todoTask.id !== Number(req.params.taskid));
 });
 module.exports = router;
